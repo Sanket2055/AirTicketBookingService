5ddb56b629f53aa68c5169fa9ac0865af88c94e5
Sanket2055 <sanketdiwate95@gmail.com>
2023-10-03 22:16:10 +0530

    Readme Updated

---
+++ Readme.md
@@ -0,0 +1,11 @@
+1. [Auth Service](https://github.com/Sanket2055/Auth_Service)
+
+2. [Reminder Service](https://github.com/Sanket2055/ReminderService)
+
+3. [Flights Search Service](https://github.com/Sanket2055/Flights-Service)
+
+### API GATEWAY
+
+<!-- https://github.com/Sanket2055/API_Gateway -->
+
+1. [API Gateway](https://github.com/Sanket2055/API_Gateway)
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ const bodyParser = require("body-parser");
 const { PORT, DB_SYNC, FLIGHT_SERVICE_PATH } = require("./config/serverConfig");
 const apiRoutes = require("./routes/index");
 const db = require("./models/index")
+const morgan = require("morgan");
+
 const setUpandStartServer = async () => {
     // ... other code
-
+    app.use(morgan("combined"))
+    app.get("/bookingservice/api/v1/home", (req, res) => {
+        res.send("Welcome to booking service")
+    })
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
-
-    app.use("/api", apiRoutes);
+    app.use("/bookingservice/api", apiRoutes);
 
     app.listen(PORT, () => {
         console.log("Server is listening on port:" + PORT);
